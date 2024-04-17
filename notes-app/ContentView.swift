558d8a9b49d442bf9d2930f64b0c2ee00734db96
Lekau Mamabolo <lekau9222@gmail.com>
2024-04-17 22:29:34 +0200

    First view plus seach bar

--- notes-app/ContentView.swift
+++ notes-app/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchTerm: String = ""
+    @State private var notes: [String] = [
+        "Buy groceries: Milk, eggs, bread",
+        "Walk the dog at 7pm",
+        "Call mom for her birthday",
+        "Finish reading that book chapter",
+        "Pay the electricity bill",
+        "Schedule dentist appointment",
+        "Fix the leaky faucet in the bathroom",
+        "Water the plants",
+        "Pick up dry cleaning",
+        "Return library books",
+        "Meet friend for lunch",
+        "Go for a run",
+        "Do laundry",
+        "Clean the kitchen",
+        "Watch that new movie everyone's talking about",
+        "Write a thank-you note",
+        "Organize your closet",
+        "Meditate for 10 minutes",
+        "Practice playing the guitar",
+        "Learn a new recipe"
+    ]
+    var filteredNotes: [String] {
+        guard !searchTerm.isEmpty else { return notes}
+        return notes.filter { $0.localizedCaseInsensitiveContains(searchTerm)}
+    }
+    var colour: [Color] = [.blue, .green, .yellow, .purple, .orange]
+    private var adaptiveColumns = [GridItem(.adaptive(minimum: 170))]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 20,
+                          content: {
+                    ForEach(filteredNotes, id:\.self) { note in
+                        NavigationLink(
+                            destination: notes_detail_view(),
+                            label: {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 170, height: 170)
+                                    .foregroundColor(colour[Int.random(in: 1 ..< 5)])
+                                    .cornerRadius(10)
+                                Text(note).foregroundColor(.black)
+                                    .multilineTextAlignment(.center).padding()
+                            }
+                        })
+                    }
+                })
+                
+            }
+        }.searchable(text: $searchTerm, prompt: "Search For Notes")
     }
 }
 
---
+++ notes-app/notes_detail_view.swift
@@ -0,0 +1,18 @@
+//
+//  notes_detail_view.swift
+//  notes-app
+//
+//  Created by Lekau on 2024/04/17.
+//
+
+import SwiftUI
+
+struct notes_detail_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    notes_detail_view()
+}
