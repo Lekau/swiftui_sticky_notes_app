3af58a7c431f0e2dc7fe82b6e655e4f90c6e302e
Lekau Mamabolo <lekau9222@gmail.com>
2024-04-17 20:39:45 +0200

    Initial Commit

---
+++ notes-app/notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_appApp.swift
+//  notes-app
+//
+//  Created by Lekau on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
